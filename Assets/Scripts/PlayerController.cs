df19313e7d9982e065318c734898498477b54a99
Ramon Gonzalez Fernandez <rg2872078+github@gmail.com>
2020-10-21 07:43:59 -0400

    4.5,5 side to side movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	public float horizontalInput;
+	public float speed = 10.0f;
 
 	private void Start()
 	{
@@ -12,5 +13,6 @@ public class PlayerController : MonoBehaviour
 	private void Update()
 	{
 		horizontalInput = Input.GetAxis("Horizontal");
+		transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 	}
 }
