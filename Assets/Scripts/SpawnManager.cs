01ba0dca8d6d51c6c3a61344ed2ad1b04b8d6c49
Ramon Gonzalez Fernandez <rg2872078+github@gmail.com>
2020-11-13 18:54:52 -0500

    spawn animals in separate function

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -15,11 +15,16 @@ public class SpawnManager : MonoBehaviour
 	{
 		if (Input.GetKeyDown(KeyCode.S))
 		{
-			Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-			int animalIndex = Random.Range(0, animalPrefabs.Length);
-			Instantiate(animalPrefabs[animalIndex],
-						spawnPos,
-						animalPrefabs[animalIndex].transform.rotation);
+			SpawnRandomAnimal();
 		}
+	}
+
+	private void SpawnRandomAnimal()
+	{
+		Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+		int animalIndex = Random.Range(0, animalPrefabs.Length);
+		Instantiate(animalPrefabs[animalIndex],
+					spawnPos,
+					animalPrefabs[animalIndex].transform.rotation);
 	}
 }
